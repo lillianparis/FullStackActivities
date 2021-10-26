972826638df3973f7700944309950b5fe4156d4d
Lillian Paris <lillian.paris529@gmail.com>
2021-10-26 13:16:08 -0700

    Adding

--- Number-3/unsolved/README.md
+++ Number-3/unsolved/README.md
@@ -1,3 +1,3 @@
 # Number-3
 
-- Event Handling
+- Hover Effects and Tooltips
---
+++ Number-4/solved/script.js
@@ -0,0 +1,15 @@
+const width = 500;
+const height = 500;
+
+//Create SVG element
+const svg = d3.select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+
+svg.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 200)
+        .attr("height", 100)
