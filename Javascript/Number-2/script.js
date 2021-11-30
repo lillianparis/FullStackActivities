98c1ea7618a1ecc33acd08af78d740a39fae0c1e
Lillian Paris <lillian.paris529@gmail.com>
2021-11-30 14:01:55 -0700

    Added js

---
+++ Javascript/Number-2/script.js
@@ -0,0 +1,18 @@
+// Declaring Variables
+// Variables are used to store data values. Javascript uses the keywords var, let and const to declare variables. An equal sign is used to assign values to variables.
+// Don't worry we will go more into let, var and const later!
+
+// Declaring the variable
+const shrek;
+
+// Storing the value of PG in the variable shrek 
+
+shrek = PG;
+
+// Assigning the value of one variable to another
+
+const shrekTwo;
+
+// This is called the assignment operator
+
+shrekTwo = shrek;
