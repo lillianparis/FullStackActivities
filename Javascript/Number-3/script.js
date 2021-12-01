d5c2846b03d204478e7e000d38306b86cfe81169
Lillian Paris <lillian.paris529@gmail.com>
2021-12-01 11:13:29 -0700

    Added in Number-3 to Javascript

---
+++ Javascript/Number-3/script.js
@@ -0,0 +1,16 @@
+// Some useful functions (Examples)
+
+// The alert() function pops up a window with some text:
+alert("Hello World");
+
+// The console.log() function writes some text into the console, which is hidden in the inspector:
+console.log("Hello this is the console!")
+
+// The prompt() function can have a default value
+const pass = prompt("This is a prompt with the variable name of pass")
+
+// The parseInt() function converts a string of digits to a number:
+const baking = prompt("How many pastries");
+const b = parseInt(how_many);
+alert(b+"Pastries!");
+
